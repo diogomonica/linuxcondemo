2e40ce34ea70cd7d2f98156782fc9e5db2f2e67f
Diogo Monica <diogo@docker.com>
2015-08-16 11:08:17 -0700

    Self installing demo.sh

--- demo.sh
+++ demo.sh
@@ -1,15 +1,34 @@
 #! /bin/sh
 
-#docker build --rm --force-rm -t diogomonica/notarytest .
+# Build and run notary
+git clone -b trust-sandbox https://github.com/docker/notary.git
+cd notary; docker-compose kill; docker-compose rm; docker-compose build && docker-compose up -d
+cd ..
+
+# Build and run registry
+git clone https://github.com/docker/distribution.git
+cp Dockerfile.registry distribution/Dockerfile
+cd distribution; docker build -t registry .
+docker kill registry; docker rm registry; docker run -d -p 5000:5000 --name registry registry
+cd ..
+
+# Build and push the sandbox
+docker build --rm --force-rm -t diogomonica/notarytest .
 #docker push diogomonica/notarytest
+
+# This stuff is to test with simple alpine instead of dolly
 #docker build --rm --force-rm -t registry:5000/demo -f Dockerfile.alpine .
 #docker build --rm --force-rm -t registry:5000/demo_malicious -f Dockerfile_malicious.alpine .
 #docker push registry:5000/demo_malicious
 #docker push registry:5000/demo
+#docker images | grep demo | awk '{print $3}' | xargs docker rmi -f
+
+# Build and push dolly and malicious dolly
 docker build --rm --force-rm -t registry:5000/dolly -f Dockerfile.nginx .
 docker push registry:5000/dolly
 docker build --rm --force-rm -t registry:5000/dolly_malicious -f Dockerfile_malicious.nginx .
 docker push registry:5000/dolly_malicious
+
 #docker images | grep demo | awk '{print $3}' | xargs docker rmi -f
 docker images | grep dolly | awk '{print $3}' | xargs docker rmi -f
 docker run -it -v /var/run/docker.sock:/var/run/docker.sock --link notary_notaryserver_1:notaryserver --link registry:registry diogomonica/notarytest
